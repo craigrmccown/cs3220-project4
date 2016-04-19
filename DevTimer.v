bc1059c4f3229cf6d78891000187189dfb41afff
Craig McCown <craigrmccown@gmail.com>
2016-04-19 01:37:05 -0400

    timer test working

--- DevTimer.v
+++ DevTimer.v
@@ -19,9 +19,9 @@ module DevTimer(CLK, RESET, ABUS, WE, DBUS_IN, DBUS_OUT);
 	wire ctrlActive = ABUS == CTRLADDR;
 	
 	wire readLimit = !WE && limitActive;
-	//wire writeLim = WE && limActive;
+	wire writeLimit = WE && limitActive;
 	wire readCount = !WE && countActive;
-	//wire writeCnt = WE && cntActive;
+	wire writeCount = WE && countActive;
 	wire readCtrl = !WE && ctrlActive;
 	wire writeCtrl = WE && ctrlActive;
 	
@@ -29,25 +29,44 @@ module DevTimer(CLK, RESET, ABUS, WE, DBUS_IN, DBUS_OUT);
 		if (RESET) begin
 			ticks <= 0;
 			count <= 0;
-			limit <= 32'd10;
+			limit <= 0;
 			ready <= 1'b0;
 			overflow <= 1'b0;
 		end else begin
-			if (ticks == (MSTICKS - 1)) begin
+			if (writeLimit) begin
+				limit <= DBUS_IN;
+				count <= 0;
 				ticks <= 0;
-				
-				if (count == (limit - 1) && limit != 0) begin
-					count <= 0;
-					ready <= 1'b1;
-				end else begin
-					count <= count + 1;
-				end
+			end else if (writeCount) begin
+				count <= DBUS_IN;
+				ticks <= 0;
 			end else begin
-				ticks <= ticks + 1;
-			end
-			
-			if (writeCtrl) begin
-				{overflow, ready} <= DBUS_IN[1 : 0];
+				if (ticks == (MSTICKS - 1)) begin
+					ticks <= 0;
+					
+					if (count == (limit - 1) && limit != 0) begin
+						count <= 0;
+						ready <= 1'b1;
+						
+						if (ready)
+							overflow <= 1'b1;
+					end else begin
+						count <= count + 1;
+					end
+				end else begin
+					ticks <= ticks + 1;
+				end
+				
+				if (writeCtrl) begin
+					if (!DBUS_IN[0])
+						ready <= 1'b0;
+						
+					if (!DBUS_IN[1])
+						overflow <= 1'b0;
+				end else if (readCount) begin
+					ready <= 1'b0;
+					overflow <= 1'b0;
+				end
 			end
 		end
 	end
